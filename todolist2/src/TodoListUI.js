a8a589163387ef15ebdaadb0d785ce64596a4e1d
shawn <xx090304@163.com>
2018-10-04 19:43:48 +0800

    无状态组件

--- todolist2/src/TodoListUI.js
+++ todolist2/src/TodoListUI.js
@@ -2,29 +2,26 @@ import React,{Component,Fragment} from 'react';
 import 'antd/dist/antd.css';
 import { Input,Button,List } from 'antd';
 
-class TodoListUI extends Component{
-
-    render(){
-        return <Fragment>
+const TodoListUI = (props)=>{
+    return (<Fragment>
         <div style={{margin:10}}>
-          <div>
-          <Input 
-          placeholder="Basic usage" 
-          value={this.props.inputValue}
-          onChange={this.props.handleChangeValue}
-          style={{width:300,marginRight:10}}
-          />
-             <Button type="primary" onClick={this.props.handleButtonClick}>提交</Button>
-          </div>
-          <List
+        <div>
+        <Input 
+        placeholder="Basic usage" 
+        value={props.inputValue}
+        onChange={props.handleChangeValue}
+        style={{width:300,marginRight:10}}
+        />
+            <Button type="primary" onClick={props.handleButtonClick}>提交</Button>
+        </div>
+        <List
             style={{marginTop:'30px',width:'300px'}}
             bordered
-            dataSource={this.props.list}
-            renderItem={(item,index) => (<List.Item  onClick={(index)=>{this.props.handleListClick(index)}}>{item}</List.Item>)}
+            dataSource={props.list}
+            renderItem={(item,index) => (<List.Item  onClick={(index)=>{props.handleListClick(index)}}>{item}</List.Item>)}
             />
         </div>
-    </Fragment>
-    }
+    </Fragment>)
 }
 
 export default TodoListUI;
