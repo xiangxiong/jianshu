7fa14de672e134b9c5feaf2580b2e4080464df23
shawn <xx090304@163.com>
2018-10-05 11:56:59 +0800

    使用 Immutable.js 来管理store中的数据

--- my-app/src/common/header/index.js
+++ my-app/src/common/header/index.js
@@ -31,7 +31,7 @@ const Header = (props) =>{
 
 const mapStateToProps = (state)=>{
     return {
-        focused:state.header.focused
+        focused:state.header.get('focused')
     }
 }
 
--- my-app/src/common/header/store/reducer.js
+++ my-app/src/common/header/store/reducer.js
@@ -1,14 +1,18 @@
 import * as actionTypes from './constants';
+import {fromJS} from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
     focused:false
-};
+});
 
 export default (state=defaultState,action)=>{
     if(action.type === actionTypes.USER_REGISTER_ACTION){
-        const newState = JSON.parse(JSON.stringify(state));
-        newState.focused = action.focused;
-        return newState;
+        // const newState = JSON.parse(JSON.stringify(state));
+        // newState.focused = action.focused;
+        // return newState;
+        // immutable 对象的set方法,会结合之前 immutable 对象的值.
+        // 和设置的值, 返回一个全新的对象.
+        return state.set('focused',true);
     }
     return state;
 }
