645a6946819a354f7cba3bf214f236578a6d8f07
shawn <xx090304@163.com>
2018-10-05 21:30:17 +0800

    首页专题区域布局及reducer的设计

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component,Fragment } from 'react';
 import Header from './common/header';
 import store from './store/';
 import {Provider} from 'react-redux';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import {BrowserRouter,Route} from 'react-router-dom';
 
 class App extends Component {
@@ -12,8 +14,8 @@ class App extends Component {
         <Header/>
           <BrowserRouter>
             <Fragment>
-              <Route path='/' exact render={()=><div>home</div>}></Route>
-              <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+              <Route path='/' exact component={Home}></Route>
+              <Route path='/detail' exact component={Detail}></Route>
             </Fragment>
           </BrowserRouter>
        </Fragment>
